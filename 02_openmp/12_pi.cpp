b2c9cbb1fff16f95380ad4bd0457d7eb2ad733e8
iduxinyu <iduxinyu.sing@gmail.com>
2024-04-22 00:02:53 +0900

    24M31511 homework

--- 02_openmp/12_pi.cpp
+++ 02_openmp/12_pi.cpp
@@ -1,12 +1,17 @@
 #include <cstdio>
-
+#include <omp.h>
 int main() {
   int n = 10;
   double dx = 1. / n;
   double pi = 0;
-  for (int i=0; i<n; i++) {
+  int i;
+#pragma omp parallel for reduction(+:pi)
+  for (i=0; i<n; i++) {
     double x = (i + 0.5) * dx;
     pi += 4.0 / (1.0 + x * x) * dx;
+    //printf("loop %d: thread_num %d\n",i,omp_get_thread_num());
+    
   }
+
   printf("%17.15f\n",pi);
 }
--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+#include <omp.h>
 
 int main() {
   int n = 50;
@@ -12,12 +13,41 @@ int main() {
   }
   printf("\n");
 
+  
   std::vector<int> bucket(range,0); 
+  
+
   for (int i=0; i<n; i++)
     bucket[key[i]]++;
   std::vector<int> offset(range,0);
+
+ /* printf("bucket: ");
+  for(int i=0;i<range;i++)
+	  printf("%d ",bucket[i]);
+  printf("\n");
+*/
   for (int i=1; i<range; i++) 
-    offset[i] = offset[i-1] + bucket[i-1];
+    //offset[i] = offset[i-1] + bucket[i-1];
+    offset[i]+=offset[0];
+
+  #pragma omp parallel
+  {
+    for(int j=1;j<range;j++)
+    {
+      #pragma omp for
+      for(int i=j;i<range;i++){
+	      offset[i]+=bucket[i-j];
+             // printf("offset[%d]:%d  threads:%d\n",i,offset[i] ,omp_get_thread_num());
+
+      }
+    }
+  }
+
+/*  printf("offset ");
+for(int i=0;i<range;i++)
+	printf("%d ",offset[i]);
+printf("\n");
+*/
   for (int i=0; i<range; i++) {
     int j = offset[i];
     for (; bucket[i]>0; bucket[i]--) {
